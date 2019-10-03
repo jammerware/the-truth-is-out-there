6fe06c0fb136bc0114d0eef158dec9ec6968a15a
Ben Stein <ben@jammerware.com>
2019-10-03 16:37:02 -0400

    fixing webpack/ngbuild weirdness

--- src/app/services/chrome.service.ts
+++ src/app/services/chrome.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
+import 'chrome';
 
 @Injectable({ providedIn: 'root' })
 export class ChromeService {
-
-    constructor() { }
-
     getExtensionUrl(path: string) {
         console.log(chrome.extension.getURL(path));
         return chrome.extension.getURL(path);
